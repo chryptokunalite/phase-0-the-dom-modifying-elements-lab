44a40b02ee842bd2b9084d78f87c9f148845d2b5
Allan Saenz <allan.d.saenz@gmail.com>
2023-06-25 20:48:04 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "Allan Saenz is the champion"
+document.body.append(newHeader)
+
